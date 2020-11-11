a5f0c7932857b2e90138d1e04843f558719f8986
kai-blt <blaitong@gmail.com>
2020-11-11 08:31:06 -0800

    added ability to add movie

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import UpdateMovie from './Movies/UpdateMovie'
+import AddMovie from './Movies/AddMovie';
 import Movie from "./Movies/Movie";
 import axios from 'axios';
 
@@ -39,6 +40,10 @@ const App = () => {
 
       <Route path="/update-movie/:id">
         <UpdateMovie getMovieList={getMovieList}/>
+      </Route>
+
+      <Route path="/add-movie">
+        <AddMovie getMovieList={getMovieList}/>
       </Route>
     </>
   );
---
+++ client/src/Movies/AddMovie.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from "react-router-dom";
+import axios from 'axios';
+
+
+//Initial form values
+const initialValues = {
+  id: null,
+  title: '',
+  director: '',
+  metascore: null,
+  stars: []
+}
+
+const AddMovie = (props) => {
+    const [formValues, setFormValues] = useState(initialValues);
+    const params = useParams();
+    const history = useHistory();
+
+  
+    //handlers
+    const onSubmit =(e) => {
+        e.preventDefault();
+        axios
+        .post(`http://localhost:5000/api/movies`, formValues)
+            .then(res => {
+                setFormValues(initialValues);
+                props.getMovieList();
+                history.push('/');
+            })
+            .catch(err => console.log(err.response));
+    }
+
+    const handleChange = (e) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return(
+        <div className="update-movie-card">            
+            <form onSubmit={onSubmit}>
+                <h2>Add Movie</h2><br/>
+                <label>Title
+                    <input
+                        type="text"
+                        name="title"
+                        value={formValues.title}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>Director
+                    <input
+                        type="text"
+                        name="director"
+                        value={formValues.director}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>Metascore
+                    <input
+                        type="number"
+                        name="metascore"
+                        value={formValues.metascore}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>Actors
+                    <input
+                        type="text"
+                        name="stars"
+                        value={formValues.stars}
+                        onChange={handleChange}
+                    />
+                </label>
+                <button>Accept Changes</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddMovie;
